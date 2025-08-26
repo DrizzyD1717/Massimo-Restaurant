6bf049b5c4c9a0d16b6aa8095fb94a93b6fea877
Olulana Dari <dariolulana@gmail.com>
2025-08-26 12:13:16 +0100

    I completed the Pizza Menu which shows all pizza products

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,7 +1,40 @@
+import { pizzas } from "@/data";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const categoryPage = () => {
-  return <div>categoryPage</div>;
+  return (
+    <div className="flex flex-wrap text-red-500">
+      {pizzas.map((item) => (
+        <Link
+          href={`/product/${item.id}`}
+          className={`w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group even:bg-fuchsia-50`}
+          key={item.id}
+        >
+          {/* Image Container */}
+          {item.img && (
+            <div className="relative h-[80%]">
+              <Image
+                src={item.img}
+                fill
+                alt=""
+                className="object-contain"
+              ></Image>
+            </div>
+          )}
+          {/* Text Container */}
+          <div className="flex items-center justify-between font-bold ">
+            <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+            <h2 className="group-hover:hidden text-xl ">${item.price}</h2>
+            <button className=" hidden uppercase bg-red-500 text-white p-2 rounded-md group-hover:block">
+              Add To Cart
+            </button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default categoryPage;
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 const singleProduct = () => {
-  return <div>singleProduct</div>;
+  return (
+    <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row">
+      {/* Image Continer */}
+      <div className="relative">
+        <Image src="" fill alt="" className="object-contain"></Image>
+      </div>
+    </div>
+  );
 };
 
 export default singleProduct;
